d6d852b1497aba24ae492ba6099561406984ea6d
Ghadeer Elsharkawy <ghadeer.sharkawy@gmail.com>
2020-01-27 13:07:57 +0200

    add bootstrap

---
+++ src/components/blogHeader/BlogHeader.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="blogHeader">
+    <b-navbar toggleable="lg" type="dark" variant="info">
+    <b-navbar-brand href="#">Vue Blog</b-navbar-brand>
+
+    
+
+      <!-- Right aligned nav items -->
+      <b-navbar-nav class="ml-auto">
+        <b-nav-item-dropdown right>
+       <b-collapse id="nav-collapse" is-nav>
+      <b-navbar-nav>
+        <b-nav-item href="#">Link</b-nav-item>
+        <b-nav-item href="#" disabled>Disabled</b-nav-item>
+      </b-navbar-nav>
+      </b-collapse>
+  </b-navbar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BlogHeader'
+};
+</script>
